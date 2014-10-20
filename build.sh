d5073d913c4fe8fe79992224590452ad10e412b3
Igor Polevoy <igor@expresspigeon.com>
2014-10-20 14:27:59 -0500

    removed toc from pandoc script

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 rm -rf output/*
-for file in `ls src/activejdbc/*.md | tr '/' ' ' | awk '{print $3}'`; do  pandoc -f markdown -t html --template template.html  src/activejdbc/$file -o output/$file.html; done
-for file in `ls src/activeweb/*.md | tr '/' ' ' | awk '{print $3}'`; do  pandoc -f markdown -t html --template template.html src/activeweb/$file -o output/$file.html; done
+for file in `ls src/activejdbc/*.md | tr '/' ' ' | awk '{print $3}'`; do  pandoc -f markdown -t html   src/activejdbc/$file -o output/$file.html; done
+for file in `ls src/activeweb/*.md | tr '/' ' ' | awk '{print $3}'`; do  pandoc -f markdown -t html src/activeweb/$file -o output/$file.html; done
 
 cp src/activejdbc/*.properties output/
 cp src/activeweb/*.properties output/
